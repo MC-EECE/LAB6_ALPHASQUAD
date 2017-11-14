ac0b028291b1ecffde21695f2cbad47d20cff9d3
toakes59 <31904863+toakes59@users.noreply.github.com>
2017-11-14 15:24:02 -0500

    Final commit

--- PWM.v
+++ PWM.v
@@ -1,16 +1,26 @@
-module PWM_counter #(parameter pulse_width = 100 ) (input clk, input rst, output reg[3:0] count);
-always @ (posedge clk)
-begin 
-if (count < pulse_width)
-count=count+1'b1;
-else
-count= 4'b0;
+module PWM_counter (input wire [7:0] pulse_width, input wire [7:0] pulse_period, input wire clk, input wire rst_n, output wire pwm );
+reg d, q;
+reg [3:0] count_q,count_d;
+assign pwm = q;
+assign rst = ~rst_n;
+
+always @ (*)
+begin
+	count_d = count_q + 1'b1; 
+if (count_q > pulse_period)
+	count_d = 0;
+	d = (count_d< pulse_width)?1'b1:1'b0;
 end
 
-always @ (negedge rst)
-begin
-if (rst = 1)
-count= 4'b0;
+always @ (posedge clk)begin
+if (rst) begin
+	count_q <= 8'b0;
+end 
+else begin
+	count_q <= count_d;
 end
+	q<=d;
+end
+assign pwm = q;
 
 endmodule
