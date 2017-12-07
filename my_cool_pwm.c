b76da26f6c28c76cba1c049c8b8423df433b6122
toakes59 <31904863+toakes59@users.noreply.github.com>
2017-12-06 20:32:05 -0500

    SOS

--- my_cool_pwm.c
+++ my_cool_pwm.c
@@ -20,12 +20,14 @@ int main(void)
 { 
 	alt_u32 period, pulse_width;
 	alt_u8 enable = 0;
-	IOWR(PWM_0_BASE, 3, enable);
+	IOWR(PWM_0_BASE, PWM_enable, enable);
 	while(1) {
-    // read from switches to determine pulse width
-	pulse_width = IORD(SLIDER_SWITCHES_BASE,4);
+
+	// read from switches to determine pulse width
+	enable = (IORD(SLIDER_SWITCHES_BASE, 0)&0x20000)?1:0;
+	pulse_width = IORD(SLIDER_SWITCHES_BASE,0)&0xFF;
 	// set the pwm period
-	period = IORD(SLIDER_SWITCHES_BASE, 0);
+	period = (IORD(SLIDER_SWITCHES_BASE,0)&0xFF00) >> 8;
 
 	// output the period to the pwm
 	// output the pulse width value to the pwm
@@ -34,9 +36,9 @@ int main(void)
     IOWR(PWM_0_BASE, 2, pulse_width);
 
 	// make sure the pwm is enabled
-    if (enable=0)
+    if (enable==0){
     	enable = 1;
-    	IOWR(PWM_0_BASE, 3, enable);
+    	IOWR(PWM_0_BASE, PWM_enable, enable);}
 	}
     return 0;
 }
